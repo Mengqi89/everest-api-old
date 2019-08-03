c9e36380b73b2b110690b427842bfe55c06390be
nataliesun <natsun121@gmail.com>
2019-08-02 23:43:40 -0700

    added auth router for schools and authentication for school password

--- src/schools/schools-service.js
+++ src/schools/schools-service.js
@@ -1,4 +1,15 @@
+const bcrypt = require('bcryptjs')
+const xss = require('xss')
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+
 const SchoolsService = {
+  hasUsername(db, username) {
+    return db('everest_schools')
+      .where({ username })
+      .first()
+      .then(username => !!username)
+  },
   getAllSchools(knex) {
     return knex.select('*').from('everest_schools')
   },
@@ -32,7 +43,24 @@ const SchoolsService = {
       .where({ id })
       .update(newSchoolFields)
   },
-
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password be longer than 8 characters'
+    }
+    if (password.length > 72) {
+      return 'Password be less than 72 characters'
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces'
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain one upper case, lower case, number and special character'
+    }
+    return null
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12)
+  },
   serializeSchool(school) {
     return {
       username: school.username,
