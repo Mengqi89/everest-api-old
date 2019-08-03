ae552b11d87f36dcb652be563bab81c232c4ccd5
nataliesun <natsun121@gmail.com>
2019-08-02 20:35:34 -0700

    added routers and config, and added username and pass to serialize school

--- src/app.js
+++ src/app.js
@@ -2,31 +2,31 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config');
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const schoolsRouter = require('./schools/schools-router')
+
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(cors())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
+app.use(cors());
 app.use(helmet())
 
-app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
-})
+app.use('/api/schools', schoolsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { error: error.message, object: error }
+  }
+  res.status(500).json(response)
+})
+
 module.exports = app
--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    PORT: process.env.PORT || 8000, // feeds into server.js
-    NODE_ENV: process.env.NODE_ENV || 'development', //feeds into app.js
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://everest@localhost:5432/everest',
+  JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:8000/api",
+  CLIENT_ORIGIN: 'http://localhost:3000',
+}
