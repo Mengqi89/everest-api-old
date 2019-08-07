b048657cef890c2593e59f462b03d1c73ae96e39
Mengqi Wang <wmq516@gmail.com>
2019-08-07 14:26:16 -0600

    added everest_application table

---
+++ migrations/005.do.create_everest_applications.sql
@@ -0,0 +1,6 @@
+CREATE TABLE everest_applications
+(
+    id SERIAL PRIMARY KEY,
+    job INTEGER REFERENCES everest_jobs(id),
+    teacher INTEGER REFERENCES everest_teachers(id)
+);
--- seeds/seed.everest.sql
+++ seeds/seed.everest.sql
@@ -1,10 +1,29 @@
 BEGIN;
 
   TRUNCATE
+  everest_admins,
   everest_schools,
-  everest_teachers
+  everest_jobs,
+  everest_teachers,
+  everest_applications
   RESTART IDENTITY CASCADE;
-
+  
+  INSERT INTO everest_admins 
+  (
+    first_name,
+    last_name,
+    username,
+    email,
+    password
+    )
+    VALUES
+    (
+    'Dunder',
+    'Mifflin',
+    'dunder',
+    'wmq516@gmail.com',
+    '!Aa101010'
+    );
 
   INSERT INTO everest_schools
     (
@@ -94,6 +113,66 @@ BEGIN;
       3,
       'Restaurants, shopping, bars'
     );
+    
+    INSERT INTO everest_jobs
+    (
+    job_title,
+    course,
+    grade_level,
+    textbook_used,
+    number_of_courses_to_teach,
+    number_of_sections,
+    max_class_size,
+    total_hours_of_class_per_week,
+    extra_duties_required,
+    hours_of_extra_duties_per_week,
+    minimum_degree_required,
+    preferred_degree,
+    minimum_experience_required,
+    preferred_experience_level,
+    native_english_speaker,
+    other_qualification,
+    base_pay_per_month,
+    bonuses,
+    plane_ticket_provided_to_china,
+    plane_ticket_provided_from_china,
+    plane_ticket_reimbursment,
+    paid_time_off,
+    sick_days,
+    personal_days,
+    time_off_for_holidays,
+    total_salary
+    )
+    VALUES
+    (
+    'English Instructor',
+    'Honors English',
+    '11th grade',
+    NULL,
+    2,
+    6,
+    25,
+    20,
+    'office hours',
+    5,
+    'BA',
+    'MA',
+    1,
+    3,
+    true,
+    'preferred experience teaching abroad',
+    10000,
+    true,
+    true,
+    true,
+    false,
+    false,
+    10,
+    5,
+    false,
+    15000
+    );
+
   INSERT INTO everest_teachers
     (
     username,
