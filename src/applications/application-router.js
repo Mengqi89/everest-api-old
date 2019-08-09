9b82e832859eff37d20bb3ae8b6c5c9808e0e761
Mengqi Wang <wmq516@gmail.com>
2019-08-09 14:43:49 -0600

    added endpoint for approve application, refactored endpoint for get /:applicationId

--- src/applications/application-router.js
+++ src/applications/application-router.js
@@ -9,16 +9,24 @@ applicationsRouter
         ApplicationsService.getAllApplications(req.app.get('db'))
             .then(applications => res.json(applications))
             .catch(next)
-
     })
 
+
 applicationsRouter
-    .route('/:jobId/:teacherId')
+    .route('/:applicationId')
     .get((req, res, next) => {
-        const jobId = req.params.jobId
-        const teacherId = req.params.teacherId
-        ApplicationsService.getApplicationById(req.app.get('db'), jobId, teacherId)
+        const applicationId = req.params.applicationId
+        ApplicationsService.getApplicationById(req.app.get('db'), applicationId)
             .then(application => res.json(application))
+            .catch(next)
+    })
+    .patch((req, res, next) => {
+        const applicationId = req.params.applicationId
+        const newApplicationField = {
+            application_approved: true
+        }
+        ApplicationsService.approveApplication(req.app.get('db'), applicationId, newApplicationField)
+            .then(numRowsAffected => res.json(numRowsAffected))
             .catch(next)
     })
 
