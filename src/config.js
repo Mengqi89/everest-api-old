a3aca0139df8067486793df3eaa9b1b0b8f173f9
Mengqi Wang <wmq516@gmail.com>
2019-08-21 13:26:44 -0600

    removed Procfile

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:8000/api",
-  CLIENT_ORIGIN: 'http://localhost:3000',
+  CLIENT_ORIGIN: 'https://everest.everest.now.sh',
 }
