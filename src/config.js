96fd16d936c7119f407a0f616b4ed19a0ae028d2
Mengqi Wang <wmq516@gmail.com>
2019-08-22 15:49:57 -0600

    completed auth-router.spec.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres:ilki@localhost/everest',
   JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:8000/api",
   CLIENT_ORIGIN: 'https://everest.everest.now.sh',
---
+++ test/test-helpers.js
@@ -0,0 +1,226 @@
+const bcrypt = require('bcryptjs')
+
+function makeAdminArray() {
+    return [
+        {
+            'id': 1,
+            'first_name': 'test1',
+            'last_name': 'test1',
+            'username': 'test1',
+            'email': 'test1@test.net',
+            'password': '!wW101010'
+        },
+        {
+            'id': 2,
+            'first_name': 'test2',
+            'last_name': 'test2',
+            'username': 'test2',
+            'email': 'test2@test.net',
+            'password': '!wW101010'
+        }
+    ]
+}
+
+function makeTeacherArray() {
+    return [
+        {
+            id: 1,
+            username: 'NatBowie',
+            password: 'Password1!',
+            first_name: 'Natasha',
+            last_name: 'Bowie',
+            age: 29,
+            sex: 'Female',
+            nationality: 'UK',
+            race: 'Asian',
+            native_speaker: true,
+            married: 'no',
+            highest_degree: 'BA',
+            field_of_degree: 'English',
+            school: 'Florida State University',
+            certification: 'none',
+            years_of_experience: 1,
+            years_in_china: 0,
+            years_teaching_abroad: 1,
+        },
+        {
+            id: 2,
+            username: 'Kahmed',
+            password: 'Password1!',
+            first_name: 'Ahmed',
+            last_name: 'Kahreem',
+            age: 33,
+            sex: 'Male',
+            nationality: 'USA',
+            race: 'Other',
+            native_speaker: true,
+            married: 'yes',
+            highest_degree: 'MA',
+            field_of_degree: 'General Studies',
+            school: 'University of Alabama',
+            certification: 'TESL',
+            years_of_experience: 2,
+            years_in_china: 1,
+            years_teaching_abroad: 1,
+        }
+    ]
+
+}
+
+function makeSchoolArray() {
+    return [
+        {
+            id: 1,
+            username: 'TestSchool1',
+            password: 'passwordTestSchool1',
+            school_name: 'Test School 1',
+            school_type: 'Middle School',
+            school_size: 1300,
+            public_or_private: 'private',
+            curriculum: 'AP',
+            location: 'Chengdu',
+            notable_facts:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in suscipit tortor, eu dignissim ante. Nulla fringilla blandit ligula sit amet laoreet. Donec sagittis nisi tortor, a malesuada lacus iaculis eu. In felis est, consectetur a lorem a, efficitur hendrerit orci. Sed nibh tellus, porta id sapien in, gravida pulvinar dui. Maecenas congue dui in ante tincidunt congue. Etiam id blandit ipsum. Cras quis libero rhoncus, gravida nisi id, venenatis sapien. Integer non nunc elit. Proin hendrerit dignissim lacus, at porttitor felis pellentesque in. Donec dictum molestie lectus fringilla egestas. Aliquam erat volutpat. Curabitur ullamcorper dolor non dui accumsan elementum. Curabitur pretium, velit ac volutpat laoreet, lacus sem tempus lectus, vel dignissim nunc leo vel arcu. Morbi quis pretium magna.',
+            school_website: 'www.google.com',
+            apartment_provided: true,
+            housing_assistance: 'Will help teacher find housing.',
+            size_of_housing: '2 Bed/2 Bath',
+            // "shared_room": true,
+            // "private_room": false,
+            shared_or_private_living_space: 'Shared',
+            housing_notes: 'Shared living room with 1 other teacher',
+            housing_on_or_off: 'Off Campus',
+            time_to_get_to_class: '10 minute walk',
+            distance_to_public_trans: '10 minute walk to nearest subway stop',
+            school_charge_rent: false,
+            school_rent_notes: 'No rent',
+            school_charge_utils: false,
+            school_utils_notes: 'No charge',
+            furnished: true,
+            furnished_notes: 'Queen-size bed, writing desk, six chairs',
+            appliances_included:
+                'Refrigerator, microwave, gas stove, television, air conditioner, washing machine',
+            internet: 'Wired and wireless; wireless only; can be installed; no.',
+            computer_included: 'Desktop',
+            school_provide_health_insurance: true,
+            provided_insurance_notes: 'Up to ¥1,000,000 annual coverage; no',
+            medical_expeneses_paid: '¥1,000 deductible',
+            disability_insurance: false,
+            other_insurance: false,
+            assistance_obtaining_work_visa: true,
+            school_pay_work_visa: 'Yes',
+            school_reimburse_costs_for_obtaining_visa_in_us: true,
+            assistance_opening_chinese_bank_account: true,
+            assistance_shopping_and_settling_in: true,
+            organized_trips_for_teachers: true,
+            other_western_teachers_at_school: 3,
+            western_amenities_available_near_school_town:
+                'Restaurants, shopping, bars'
+        },
+        {
+            id: 2,
+            username: 'TestSchool2',
+            password: 'passwordTestSchool2',
+            school_name: 'Test School 2',
+            school_type: 'Middle School',
+            school_size: 2300,
+            public_or_private: 'private',
+            curriculum: 'AP',
+            location: 'Chengdu',
+            notable_facts:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in suscipit tortor, eu dignissim ante. Nulla fringilla blandit ligula sit amet laoreet. Donec sagittis nisi tortor, a malesuada lacus iaculis eu. In felis est, consectetur a lorem a, efficitur hendrerit orci. Sed nibh tellus, porta id sapien in, gravida pulvinar dui. Maecenas congue dui in ante tincidunt congue. Etiam id blandit ipsum. Cras quis libero rhoncus, gravida nisi id, venenatis sapien. Integer non nunc elit. Proin hendrerit dignissim lacus, at porttitor felis pellentesque in. Donec dictum molestie lectus fringilla egestas. Aliquam erat volutpat. Curabitur ullamcorper dolor non dui accumsan elementum. Curabitur pretium, velit ac volutpat laoreet, lacus sem tempus lectus, vel dignissim nunc leo vel arcu. Morbi quis pretium magna.',
+            school_website: 'www.google.com',
+            apartment_provided: true,
+            housing_assistance: 'Will help teacher find housing.',
+            size_of_housing: '2 Bed/2 Bath',
+            // "shared_room": true,
+            // "private_room": false,
+            shared_or_private_living_space: 'Shared',
+            housing_notes: 'Shared living room with 1 other teacher',
+            housing_on_or_off: 'Off Campus',
+            time_to_get_to_class: '10 minute walk',
+            distance_to_public_trans: '10 minute walk to nearest subway stop',
+            school_charge_rent: false,
+            school_rent_notes: 'No rent',
+            school_charge_utils: false,
+            school_utils_notes: 'No charge',
+            furnished: true,
+            furnished_notes: 'Queen-size bed, writing desk, six chairs',
+            appliances_included:
+                'Refrigerator, microwave, gas stove, television, air conditioner, washing machine',
+            internet: 'Wired and wireless; wireless only; can be installed; no.',
+            computer_included: 'Desktop',
+            school_provide_health_insurance: true,
+            provided_insurance_notes: 'Up to ¥1,000,000 annual coverage; no',
+            medical_expeneses_paid: '¥1,000 deductible',
+            disability_insurance: false,
+            other_insurance: false,
+            assistance_obtaining_work_visa: true,
+            school_pay_work_visa: 'Yes',
+            school_reimburse_costs_for_obtaining_visa_in_us: true,
+            assistance_opening_chinese_bank_account: true,
+            assistance_shopping_and_settling_in: true,
+            organized_trips_for_teachers: true,
+            other_western_teachers_at_school: 3,
+            western_amenities_available_near_school_town:
+                'Restaurants, shopping, bars'
+        }
+    ]
+}
+
+function seedAdminUsers(db, users) {
+    const preppedUsers = users.map(user => ({
+        ...user,
+        password: bcrypt.hashSync(user.password, 1)
+    }))
+    return db
+        .into('everest_admins')
+        .insert(preppedUsers)
+        .then(() =>
+            // update the auto sequence to stay in sync
+            db.raw(`SELECT setval('everest_admins_id_seq', ?)`, [
+                users[users.length - 1].id
+            ])
+        )
+}
+
+function seedTeacherUsers(db, users) {
+    const preppedUsers = users.map(user => ({
+        ...user,
+        password: bcrypt.hashSync(user.password, 1)
+    }))
+    return db
+        .into('everest_teachers')
+        .insert(preppedUsers)
+        .then(() =>
+            // update the auto sequence to stay in sync
+            db.raw(`SELECT setval('everest_teachers_id_seq', ?)`, [
+                users[users.length - 1].id
+            ])
+        )
+}
+
+function seedSchoolUsers(db, users) {
+    const preppedUsers = users.map(user => ({
+        ...user,
+        password: bcrypt.hashSync(user.password, 1)
+    }))
+    return db
+        .into('everest_schools')
+        .insert(preppedUsers)
+        .then(() =>
+            // update the auto sequence to stay in sync
+            db.raw(`SELECT setval('everest_schools_id_seq', ?)`, [
+                users[users.length - 1].id
+            ])
+        )
+}
+
+module.exports = {
+    makeAdminArray,
+    seedAdminUsers,
+    makeTeacherArray,
+    seedTeacherUsers,
+    makeSchoolArray,
+    seedSchoolUsers
+}
